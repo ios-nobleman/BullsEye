45ff342cbc38fb313e4c2eb9d1c4bb60475bc35f
Shane <shane@mymotiv.com>
2020-01-20 23:06:03 -0500

    Source Files
    Created New Project. Source Files only.

---
+++ bullsEye/bullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullsEye
+//
+//  Created by Ashley S Willbur on 1/20/20.
+//  Copyright © 2020 Ashley S Willbur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
