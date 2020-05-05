178df500d460adb795c5f2ac7f8519ab96475ccd
Shane <shane@mymotiv.com>
2020-05-05 03:26:50 -0400

    Code Formating
    Added some placeholder comments.
    Formatted the code to be more pretty.

--- bullsEye/bullsEye/ContentView.swift
+++ bullsEye/bullsEye/ContentView.swift
@@ -9,29 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var alertIsVisible: Bool = false
     
+  // Properties
+  // ==========
     
-    var body: some View {
-        VStack {
-         Text("Welcome to my first app!")
-            .fontWeight(Font.Weight.black)
-           .foregroundColor(.green)
-        Button(action: {
-            print("Button Pressed!")
-            self.alertIsVisible = true
-        }) {
+  // User interface views
+  @State var alertIsVisible: Bool = false
+    
+  // User interface content and layout
+  var body: some View {
+    VStack {
+         
+      // Target row
+      Text("Welcome to my first app!")
+        .fontWeight(Font.Weight.black)
+        .foregroundColor(.green)
+        
+      // Slider row
+      // TODO: Add views for the slider row here.
+        
+      // Button row
+      Button(action: {
+        print("Button Pressed!")
+        self.alertIsVisible = true
+       }) {
          Text("Hit Me!")
-        }
+       }
         .alert(isPresented: self.$alertIsVisible) {
-            Alert(title: Text("Hello there!"),
-                  message: Text("This is my first Pop-up."),
-                  dismissButton: .default(Text("Awesome")))
-            }
-        }
-            
+          Alert(title: Text("Hello there!"),
+                message: Text("This is my first Pop-up."),
+                dismissButton: .default(Text("Awesome")))
+       }
+        
+      // Score row
+      // TODO: Add views for the score, rounds, and start and info buttons here.
     }
+  }
+
+  // Methods
+  // =======
 }
+
+
+// Preview
+// =======
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
